271e1d3b8fb0d88717d31c085f0c0e5c3a203ff2
webfrontzhifei <906831852@qq.com>
2018-11-26 11:21:31 +0800

    feat: test成功

---
+++ test.js
@@ -0,0 +1,14 @@
+const phantom = require('phantom');
+(async function () {
+   const instance = await phantom.create();
+   const page = await instance.createPage();
+   await page.on('onResourceRequested', function(requestData){
+       console.info('Reauesting', requestData.url);
+   });
+
+   const status = await page.open('https://stackoverflow.com/');
+   const content = await page.property('content');
+   console.log(content);
+
+   await instance.exit();
+})();
